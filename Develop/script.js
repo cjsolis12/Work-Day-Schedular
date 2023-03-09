7b812343a10baf7631a79bc4b6c1db17c9170280
cjsolis <cjelizondo12@gmail.com>
2023-03-09 17:34:28 -0600

    changed day js format

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,9 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  
+
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -16,45 +18,42 @@ $(function () {
   // current hour in 24-hour time?
 
 
-var currentTime = dayjs().format('h')
-
+var currentTime = dayjs().format('H')
 console.log(currentTime)
-// ;
-//   //
-//   // TODO: Add code to get any user input that was saved in localStorage and set
-//   // the values of the corresponding textarea elements. HINT: How can the id
-//   // attribute of each time-block be used to do this?
-//   //
+
+
 for(var hour = 9; hour <=17; hour++){
   var row = $("<div>").attr('id', `hour:-${hour}`).addClass("row time-block");
-    if(hour === currentTime){
+  // Hr for each row made
+  var hourCol = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(`${hour}PM`);
+  row.append(hourCol);
+    if(hour == currentTime){
       row.addClass("present").css("color", "red")
       } else if(hour > currentTime){
       row.addClass("future").css("color", "green")
       }else{
         row.addClass("past").css("color", "grey")   
     }
-// Hr for each row made
-var hourCol = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(dayjs(hour).format('h'));
-row.append(hourCol);
-  
 
-// Description for each row made
-var description = $("<textarea>").addClass("col-8 col-md-10 description")
-row.append(description)
+  // Description for each row made
+  var description = $("<textarea>").addClass("col-8 col-md-10 description")
+  row.append(description)
 
-// Save Button for each new row 
-var saveButton = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").append($("<i>").addClass("fas fa-save").attr("aria-hidden", true));
-row.append(saveButton)
+  // Save Button for each new row 
+  var saveButton = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").append($("<i>").addClass("fas fa-save").attr("aria-hidden", true));
+  row.append(saveButton)
   
-$(".container-fluid.px-5").append(row)
+  $(".container-fluid.px-5").append(row)
 }
 
-  
+//   //
+//   // TODO: Add code to get any user input that was saved in localStorage and set
+//   // the values of the corresponding textarea elements. HINT: How can the id
+//   // attribute of each time-block be used to do this?
+//   //
 
 
-
-// TODO: Add code to display the current date in the header of the page.
+// Add code to display the current date in the header of the page.
 let today = dayjs().format('MMM DD, YYYY')
   $('#currentDay').text(today);
 
