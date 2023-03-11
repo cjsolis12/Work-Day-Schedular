5688c74b1442cbcd176be4649f1ab4ec3029b5e0
cjsolis <cjelizondo12@gmail.com>
2023-03-11 17:01:04 -0600

    fixed if statement for past, present, and future classes.colors

--- Develop/script.js
+++ Develop/script.js
@@ -9,36 +9,35 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   
+console.log("hey")
+$(".saveBtn").click(function(){
+  var textArea = $(this).prev(".description").val();
+  localStorage.setItem("calendarEvent", textArea)
+  alert("is this")
+})
 
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
 
 
 //Current Time from DayJs
-var currentTime = dayjs().format('h')
+var currentTime = dayjs().format('H')
 console.log(currentTime)
 
 for(var i = 9; i <= 17; i++){
-  var hour = i + (currentTime >= 12 ? 12 : 0)
-  var hourText = hour == 12 ? "12AM" : (hour > 12 ? `${hour - 12}PM` : `${hour}AM`);
- 
+  var hourText = i == 12 ? "12PM" : (i > 12 ? `${i - 12}PM` : `${i}AM`);
+ console.log(hourText)
   // Hr column for each row made
-  var row = $("<div>").attr('id', `hour:-${i}`).addClass("row time-block");
+  var row = $("<div>").attr('id', `hour:${i}`).addClass("row time-block");
   var hourCol = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(`${hourText}`);
   row.append(hourCol);
-
-
-    if(hour == currentTime){
-        row.addClass("present").css("color", "red")
-        } else if(hourText < currentTime){
-        row.addClass("future").css("color", "grey")
+ 
+  // Condition to check past, present, and future time blocks
+    if(i == currentTime){
+        row.addClass("present").css("background-color", "#e08071")
+        } else if(i < currentTime){
+        row.addClass("past").css("background-color", "#c6c6c3")
         }else{
-        row.addClass("past").css("color", "red")   
+        row.addClass("future").css("background-color", "#85e071")   
     }
 
   // Description for each row made
@@ -60,11 +59,6 @@ for(var i = 9; i <= 17; i++){
 
 
 // Add code to display the current date in the header of the page.
-let today = dayjs().format('MMM DD, YYYY')
+let today = dayjs().format('dddd, MMMM D')
   $('#currentDay').text(today);
-
-    setInterval(function(){
-      today = dayjs().format('MMM DD, YYYY')
-      $('#currentDay').text(today);
-    }, 1000);
 });
